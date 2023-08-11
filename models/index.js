f71fb3dd1f6e025fee08c76ce490452e0495c732
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-11 15:26:32 -0400

    Added relationships to index.js

---
+++ models/index.js
@@ -0,0 +1,37 @@
+const User = require('./User');
+const Review = require('./Review');
+const Game = require('./Game');
+
+
+
+// Each user can have many reviews:
+User.hasMany(Review, {
+    foreignKey: 'user-id'
+});
+Review.belongsTo(User, {
+    foreignKey: 'user-id'
+});
+
+
+// Each Game can have many reviews:
+    Game.hasMany(Review, {
+        foreignKey: 'game-id'
+    });
+    Review.belongsTo(Game, {
+        foreignKey: 'game-id'
+    });
+
+
+// Each user can save favorite reviews:
+// --- should be many to many and create User_Favs ---
+User.belongsToMany(Review, { through: 'User_Favs' });
+Review.belongsToMany(User, { through: 'User_Favs' });
+
+
+// Each user can save games:
+// --- should be many to many and create User_Games ---
+User.belongsToMany(Game, { through: 'User_Games' });
+Game.belongsToMany(User, { through: 'User_Games' });
+
+
+module.exports = { User, Review, Game};
