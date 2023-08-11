b8f06a22eb0a79fe82a6bb227a531ae7980a178a
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-11 16:08:01 -0400

    Added session and mysql to dependencies

---
+++ server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+// get where routes are 
+const routes = require('./controllers');
+
+// get helper functions
+const helpers = require('./utils/helpers');
+
+// to connect to mySQL
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// custom helper functions for handlebars added
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// tell Express.js the template engine (handlebars)
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
