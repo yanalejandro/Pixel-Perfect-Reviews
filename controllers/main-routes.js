cb40c215815aa4f7de325830d9fbaf9cbcf0561f
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-13 15:59:13 -0400

    Added login and signup routes

--- controllers/main-routes.js
+++ controllers/main-routes.js
@@ -7,4 +7,30 @@ router.get('/', async (req, res) => {
     res.render('dashboard');
 });
 
+// when user goes to login page
+router.get('/login', (req, res) => {
+    // if user is logged in, take them to their profile
+    if (req.session.loggedIn) {
+        res.redirect('/profile');
+        return;
+    }
+
+    // if they're not logged in, then they go to the login page
+    res.render('login');
+});
+
+// when user, doesn't have an account and wants to sign up
+router.get('/signup', (req, res) => {
+    // if user is logged in, take them to their profile
+    if (req.session.loggedIn) {
+        res.redirect('/profile');
+        return;
+    }
+
+    // currently sign up is a separate page in routes
+    // but if we want to have login and sign up together, we'll have
+    // to change this
+    res.render('signup');
+});
+
 module.exports = router;
