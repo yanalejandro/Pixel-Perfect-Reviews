d02884452a72ef51f48a759a8f11bcb9b3eadf20
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-13 15:28:25 -0400

    Added route for adding new review

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Review } = require('../../models');
+
+// add new review when given review info
+router.post('/', async (req, res) => {
+    try {
+        // create new review with given review info
+        const reviewData = await Review.create({
+            date_posted: req.body.date_posted,
+            funny: req.body.funny,
+            helpful: req.body.helpful,
+            hour_played: req.body.hour_played,
+            recommendation: req.body.recommendation,
+            review: req.body.review,
+            title: req.body.title,
+            user_id: req.session.user_id,
+            game_id: req.body.game_id
+        });
+    
+        res.status(200).json(reviewData);
+    } 
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -70,3 +70,5 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
+
+module.exports = router;
