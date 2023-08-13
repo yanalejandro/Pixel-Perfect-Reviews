acfa00fc4ff3468ed752463abc798b0826eba497
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-13 16:07:26 -0400

    Updated dashboard route to send game data

--- controllers/main-routes.js
+++ controllers/main-routes.js
@@ -2,9 +2,28 @@ const router = require('express').Router();
 const { User, Game, Review } = require('../models');
 // const Authenticate = require('../utils/authenticate');
 
-// temporary route that we can change later
+// sends game data to dashboard
+// if we want to only put the most reviewed games, we may need to edit this to only
+// grab those games
+// for now it sends all games
 router.get('/', async (req, res) => {
-    res.render('dashboard');
+    try {
+        const gameData = await Game.findAll();
+
+        // clean up data
+        const games = gameData.map((game) => game.get({plain: true}));
+
+        // sends all games and if user is logged in or not to dashboard
+        // the loggedIn is just so we can either have login or logout showing depending
+        res.render('dashboard', {
+            games,
+            loggedIn: req.session.loggedIn
+        })
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 // when user goes to login page
@@ -32,5 +51,7 @@ router.get('/signup', (req, res) => {
     // to change this
     res.render('signup');
 });
+
+
 
 module.exports = router;
