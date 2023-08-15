c7fe0960bebb9465ede6896691c4ae04a0e9fd12
Neatartist <davk0810@gmail.com>
2023-08-15 10:52:33 -0400

    added delete to the route

--- controllers/api/favoriteRoutes.js
+++ controllers/api/favoriteRoutes.js
@@ -20,3 +20,20 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// delete favorite review  
+router.delete('/:id', async (req, res) => {
+    try {
+        const favReview = await Review.findByPk(req.params.id);
+        await favReview.destroy();
+        res.status(200).json(favReview);
+    } 
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
