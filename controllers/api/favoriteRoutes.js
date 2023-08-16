2e34b92fe657c7ea242db1d8662cec8439b2fd46
Courtney Sienkiewich <sienkiewichc@gmail.com>
2023-08-16 12:06:24 -0400

    Added a lot of fixes to ui and favs

--- controllers/api/favoriteRoutes.js
+++ controllers/api/favoriteRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Review, User } = require('../../models');
 
 // add new favorite review
@@ -6,12 +7,16 @@ router.post('/', async (req, res) => {
     try {
         // grab current user
         const currentUser = await User.findByPk(req.session.user_id);
+        const user = currentUser.get({ plain: true});
 
         // grab selected review
         const favReview = await Review.findByPk(req.body.reviewId);
+        const review = favReview.get({ plain: true});
 
         // save both in junction table
-        await currentUser.addReview(favReview, { through: User_Favs });
+        await sequelize.query(`INSERT INTO user_favs (user_id, review_id) VALUES (${user.id}, ${review.id})`)
+
+        // await currentUser.addReview(favReview, { through: User_Favs });
     
         res.status(200).json(favReview);
     } 
